e898bbd7ac5c6717782f0edec1797cab70954f60
Yura Krav <krav17yura@mgail.com>
2020-11-09 11:15:08 +0200

    cart fix

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {Box, Grid, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 import CartItem from "../components/cartItem";
 import {useDispatch, useSelector} from "react-redux";
 import {minusCartItem, plusCartItem, removeCartItem} from "../redux/actions/acCart";
+import cartEmpty from './cartEmpty.svg'
+import Button from "@material-ui/core/Button";
+import {NavLink} from "react-router-dom";
 
 const useStyles = makeStyles(() => ({
 
@@ -35,14 +38,15 @@ const Cart = () => {
 
     const classes = useStyles();
     return (
-        <Grid item xs={12} lg={9} container direction="column" alignItems="center">
-            <Grid item xs={3}>
-                <Box>
-                    <Typography variant='h5' className={classes.cartTitle}>
-                        Корзина
-                    </Typography>
-                </Box>
-            </Grid>
+        <Grid item xs={12} lg={9} container direction="column" alignItems="center" justify='center'>
+            {totalCount ? <Fragment>
+                    <Grid item xs={3}>
+                        <Box>
+                            <Typography variant='h5' className={classes.cartTitle}>
+                                Корзина
+                            </Typography>
+                        </Box>
+                    </Grid>
                     {
                         itemsGroup.map(obj => <CartItem
                             {...obj}
@@ -52,18 +56,31 @@ const Cart = () => {
                             onRemoveItem={onRemoveItem}
                             onPlusItem={onPlusItem}
                             onMinusItem={onMinusItem}
-                        /> )
+                        />)
                     }
-            <Grid item xs={12} container justify={"space-around"} alignItems={"center"}>
-                    <Typography variant='h5' className={classes.cartTitle}>
-                        Всего кг фруктов: {totalCount}.
-                    </Typography>
+                    <Grid item xs={12} container justify={"space-around"} alignItems={"center"}>
+                        <Typography variant='h5' className={classes.cartTitle}>
+                            Всего кг фруктов: {totalCount}.
+                        </Typography>
 
-                    <Typography variant='h5' className={classes.cartTitle}>
-                        Сумма заказа:{totalPrice}$
-                    </Typography>
-            </Grid>
+                        <Typography variant='h5' className={classes.cartTitle}>
+                            Сумма заказа:{totalPrice}$
+                        </Typography>
+                    </Grid>
 
+                </Fragment> :
+                <Grid container direction='column' alignItems="center" justify="center">
+                    <Typography variant='h5' className={classes.cartTitle}>
+                        Корзина пустая
+                    </Typography>
+                    <img src={cartEmpty} width={300} alt=""/>
+                    <NavLink to="/" style={{textDecoration: 'none', color: 'unset'}}>
+                        <Button variant='contained' color="primary">
+                            Вернуться к покупкам
+                        </Button>
+                    </NavLink>
+                </Grid>
+            }
         </Grid>
 
     )
